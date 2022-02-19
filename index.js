34ba419be9c302e5e9b5add52130d95de1cb07b0
Arthur Jarvis <48388803+p3pi27@users.noreply.github.com>
2022-02-19 18:19:36 +0000

    feat: add more colourful title and highlight winner in gold

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-setInterval(() => {
-  r = Math.floor(Math.random() * 256);
-  g = Math.floor(Math.random() * 256);
-  b = Math.floor(Math.random() * 256);
+//const firstLoaded = Date.now();
 
-  document.getElementById("header").style.color = ("rgba(" + r + "," + g +"," + b + ",255)");
-}, 100);
+i = 0
+
+setInterval(() => {
+
+  h = i;
+
+  i = (i + 5) % 360
+
+  s = "100%";
+  l = "50%";
+
+  document.getElementById("header").style.color = ("hsl(" + h + "," + s +"," + l + ")");
+}, 50);
+
+const data = [
+  {"name": "Arthur Jarvis", "score": "5000"},
+  {"name": "Keith", "score": "9250"}
+]
 
 setTimeout(() => {
 
-  document.getElementById("leaderboard-container").innerHTML = `
-    <table class="table">
-        <thead>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Points</th>
-        </thead>
-      <tbody id="leaderboard-body">
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-        </tr>
-      </tbody>
-    </table>
+  html = `
+    <table class="table table-hover">
+      <thead>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Points</th>
+      </thead>
+    <tbody id="leaderboard-body">
   `;
+
+    for (i = 0; i < data.length; i++) {
+
+      html += `
+        <tr ${i == 0 ? 'class="table-warning"' : ""}>
+          <th scope="row">${i+1}</th>
+          <td>${data[i].name}</td>
+          <td>${data[i].score}</td>
+        </tr>
+      `;
+    }
+
+  document.getElementById("leaderboard-container").innerHTML = html;
+
 }, 1000);
