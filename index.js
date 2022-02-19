8624a712694e1e58f0bb99a76f9b71561b8a74a7
Arthur Jarvis <48388803+p3pi27@users.noreply.github.com>
2022-02-19 17:14:21 +0000

    feat: add leaderboard boilerplate

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 setInterval(() => {
-  document.getElementById("title").style.color = hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  h1.style.color = hexCodes[Math.floor(Math.random() * hexCodes.length)];
 }, 100);
+
+setTimeout(() => {
+
+  document.getElementById("leaderboard-container").innerHTML = `
+    <table class="table">
+        <thead>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Points</th>
+        </thead>
+      <tbody id="leaderboard-body">
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+        </tr>
+      </tbody>
+    </table>
+  `;
+}, 1000);
