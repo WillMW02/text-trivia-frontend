03d43ce5b51d4dc7864f442bde5e8f427dd7ad37
knightjam1 <joshball4444@gmail.com>
2022-02-20 06:20:44 +0000

    feat: rainbow text for all pages

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ setInterval(() => {
   s = "100%";
   l = "50%";
 
-  document.getElementsByClassName("header").style.color = ("hsl(" + h + "," + s +"," + l + ")");
+  //document.getElementsByClassName("changeColor").style.color = ("hsl(" + h + "," + s +"," + l + ")");
+
+  elements = document.getElementsByClassName("changeColor")
+  for(var j = 0; j < elements.length; j++){
+    elements[j].style.color = ("hsl(" + h + "," + s +"," + l + ")");
+    console.log(h)
+  }
   document.getElementById("deleteBtn").style.background = ("hsl(" + h + "," + s +"," + l + ")");
   document.getElementById("signupBtn").style.background = ("hsl(" + h + "," + s +"," + l + ")");
 }, 50);
