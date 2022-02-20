a398db3353299c02dbdc24c5b2c4858bcf5b1128
Arthur Jarvis <48388803+p3pi27@users.noreply.github.com>
2022-02-20 07:13:19 +0000

    feat: fix bug with colour changing titles

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ setInterval(() => {
   elements = document.getElementsByClassName("changeColor")
   for(var j = 0; j < elements.length; j++){
     elements[j].style.color = ("hsl(" + h + "," + s +"," + l + ")");
-    console.log(h)
   }
-  document.getElementById("deleteBtn").style.background = ("hsl(" + h + "," + s +"," + l + ")");
-  document.getElementById("signupBtn").style.background = ("hsl(" + h + "," + s +"," + l + ")");
+  //document.getElementById("deleteBtn").style.background = ("hsl(" + h + "," + s +"," + l + ")");
+  //document.getElementById("signupBtn").style.background = ("hsl(" + h + "," + s +"," + l + ")");
 }, 50);
 
 async function fetchLeaderboard() {
